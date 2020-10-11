dc69b1231e27d445e2c1a7c948b4b174be0db268
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-10-11 16:47:47 -0400

    fix(deps): depends on `p6m7g8/p6common` (#3)

--- init.zsh
+++ init.zsh
@@ -1,5 +1,9 @@
 p6df::modules::p6shell::version() { echo "0.0.1"; }
-p6df::modules::p6shell::deps() { ModuleDeps=() }
+p6df::modules::p6shell::deps() {
+  ModuleDeps=(
+    p6m7g8/p6common
+  )
+}
 p6df::modules::p6shell::init() {
 
   local dir="$P6_DFZ_SRC_DIR/p6m7g8/p6shell"
